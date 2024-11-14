0aa35635bbfd70e01f8c94de6f71ce2d618834a3
Gabo <gabo.alucema@gmail.com>
2024-11-14 01:17:52 -0300

    ajustado errores y redundancias

--- Mips.asm
+++ Mips.asm
@@ -12,9 +12,9 @@ buffer: .space 20	#espacio para el nombre del estudiante
 .globl main
 
 main:
-	evaluar_estudiante:		#bucle para multiples estudiantes
+	evaluar_estudiante:			#bucle para multiples estudiantes
 		li $v0, 4
-		la $a0, msg_nombre	#solicita nombre de estudiante
+		la $a0, msg_nombre		#solicita nombre de estudiante
 		syscall
 		
 		li $v0, 8
@@ -28,29 +28,26 @@ main:
 		
 		li $v0, 5
 		syscall
-		move $t0, $v0		#guarda el numero de notas en $t0
+		move $t0, $v0			#guarda el numero de notas en $t0
 		
-		li.s $f0, 0.0		#suma total en $f0 para flotantes
-		li $t2, 0		#inicializar contador de notas
+		li.s $f1, 0.0			# inicializar suma total en $f1 para flotantes
+		li $t2, 0				#inicializar contador de notas
 		
 	leer_notas:
 		li $v0, 4
-		la $a0, msg_nota	#solicita la nota a ingresar
+		la $a0, msg_nota		#solicita la nota a ingresar
 		syscall
 		
 		li $v0, 6
 		syscall
-		mfc1 $t3, $f0
-		add $t1, $t1, $t3
+		add.s $f1, $f1, $f0
 		addi $t2, $t2, 1
 		
 		bne $t2, $t0, leer_notas
 
 	calcular_prom:
-		mtc1 $t1, $f1
 		mtc1 $t0, $f2
 		div.s $f3, $f1, $f2
-		mfc1 $t4, $f3
 
 		li $v0, 4
 		la $a0, msg_prom
@@ -61,7 +58,7 @@ main:
 		syscall
 	
 	verificar:
-		li.s $f5, 4.0		#revisar en MARS
+		l.s $f5, 4.0			#revisar en MARS (li.s o l.s)
 		c.ls.s $f3, $f5
 		bc1t reprobar
 
